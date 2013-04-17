cc1f744e05b9353c4089442c9e6d2195e9179611
Rasmus Schultz <rasmus@mindplay.dk>
2013-04-17 15:52:42 -0400

    add a basic unit-test

--- test.php
+++ test.php
@@ -2,6 +2,8 @@
 
 header('Content-type: text/plain');
 
+#define('NOISY', true); // uncomment to enable diagnostic messages
+
 spl_autoload_register(
     function ($class) {
         include __DIR__ . '/' . $class . '.php';
@@ -19,22 +21,26 @@ class CommentModule extends Module
     {
         $this['submit'] = function ($route) {
             $route->get = function ($route, $module) {
-                echo "displaying comment submission form\n";
-                echo "module: " . get_class($module) . "\n";
-                echo "module: " . get_class($route->module) . "\n";
-                echo "module parent: " . get_class($route->module->parent) . "\n";
-                echo "module parent url: " . $module->parent->url . "\n";
-                echo "module url: " . $module->url . "\n";
+                $route->log("displaying comment submission form");
+                $route->log("module: " . get_class($module));
+                $route->log("module: " . get_class($route->module));
+                $route->log("module parent: " . get_class($route->module->parent));
+                $route->log("module parent url: " . $module->parent->url);
+                $route->log("module url: " . $module->url);
+                
+                return 'comment form';
             };
         };
         
         $this->get = function ($route, $module) {
-            echo "displaying comments!\n";
-            echo "current module: " . get_class($module) . "\n";
-            echo "module url: " . $module->url . "\n";
-            echo "module parent url: " . $module->parent->url . "\n";
-            echo "route url: " . $route->url . "\n";
-            echo "route parent url: " . $route->parent->url . "\n";
+            $route->log("displaying comments!");
+            $route->log("current module: " . get_class($module));
+            $route->log("module url: " . $module->url);
+            $route->log("module parent url: " . $module->parent->url);
+            $route->log("route url: " . $route->url);
+            $route->log("route parent url: " . $route->parent->url);
+            
+            return 'comments';
         };
     }
 }
@@ -43,82 +49,130 @@ class CommentModule extends Module
 
 $module = new Module;
 
-$module->onLog = function($message) {
-    echo "-> $message\n";
-};
+if (defined('NOISY')) {
+    $module->onLog = function($message) {
+        echo "-> $message\n";
+    };
+}
 
 $module['blog'] = function ($route) {
     $route['posts'] = function ($route) {
-        $route['(\d+)'] = function ($route, $post_id) {
+        $route['<post_id:int>'] = function ($route, $post_id) {
             if ($post_id == 99) {
                 return false; // access denied!
             }
             
             $route['edit'] = function ($route) {
                 $route->get = function ($post_id, $route) {
-                    echo "editing post {$post_id}!\n";
-                    echo "display post url: {$route->parent->url}\n";
+                    $route->log("editing post {$post_id}!");
+                    $route->log("display post url: {$route->parent->url}");
+                    
+                    return compact('post_id');
                 };
             };
             
-            $route->get = function ($post_id) {
-                echo "displaying post number {$post_id}!\n";
+            $route->get = function (Route $route, $post_id) {
+                $route->log("displaying post number {$post_id}!");
             };
             
-            $route['comments'] = function (CommentModule $comments) {
-                echo "delegating control to " . get_class($comments) . "\n";
-                echo "available vars: " . implode(', ', array_keys($comments->vars)) . "\n";
+            $route['comments'] = function (Route $route, CommentModule $comments) {
+                $route->log("delegating control to " . get_class($comments));
+                $route->log("available vars: " . implode(', ', array_keys($comments->vars)));
             };
         };
         
-        $route['(?<year>\d+)-(?<month>\d+)'] = function ($route, $year, $month) {
-            $route['page(\d+)'] = function ($route, $page) {
+        $route['<year:int>-<month:int>'] = function ($route) {
+            $route['page<page:int>'] = function ($route) {
                 $route->get = function ($route, $page, $year, $month) {
-                    echo "showing page {$page} of posts for year {$year} month {$month}\n";
-                    echo "page 1 url: " . $route->resolve('../page1')->url . "\n";
+                    $route->log("showing page {$page} of posts for year {$year} month {$month}");
+                    $route->log("page 1 url: " . $route->resolve('../page1')->url);
+                    
+                    return compact('year', 'month', 'page');
                 };
             };
             
-            $route->get = function ($year, $month) {
-                echo "showing posts from year {$year} month {$month}!\n";
+            $route->get = function ($route, $year, $month) {
+                $route->log("showing posts from year {$year} month {$month}!");
+                
+                return compact('year', 'month');
             };
         };
     };
 };
 
 $module->get = function () {
-    echo "hello from the root URL!\n";
+    return 'hello';
 };
-
-foreach (array(
-             'blog/posts/42/edit',
-             'blog/posts/2012-07',
-             'blog/posts/2012-07/page2',
-             'blog/posts/42',
-             'blog/posts/88/comments', // this will dispatch the CommentModule
-             'blog/posts/66/comments/submit', // this will dispatch inside the CommentModule
-             'blog/posts/99', // this one will fail because the router is blocking post_id 99
-             '/',
-             'blog', // this will fail because there is no method
-             'foo' // this will fail because there is no matching route
-         ) as $url) {
-    echo "\n----------------\n\nTESTING: {$url}\n\n";
-    $route = $module->resolve($url);
-    if ($route) {
-        if ($route->url) {
-            echo "RESOLVED URL: /" . ($route->url) . "\n";
-        }
-        echo "RESULT: " . ($route->execute() ? 'SUCCESS' : 'ERROR (no GET-method)') . "\n";
-    } else {
-        echo "UNRESOLVED URL: $url\n";
-    }
-}
-
-return;
 
 // Run tests:
 
 $tests = array(
+    'Resolves root URL' => function() use ($module) {
+        if ($route = $module->resolve('/')) {
+            if ($route === $module) {
+                if ($route->execute('get') === 'hello') {
+                    return true;
+                }
+            }
+        }
+    },
+    
+    'Does not resolve invalid URL' => function () use ($module) {
+        return $module->resolve('foo') === null;
+    },
+    
+    'Resolves a basic URL' => function() use ($module) {
+        if ($route = $module->resolve('blog/posts/42/edit')) {
+            if ($route instanceof Route) {
+                if ($route->execute('get') === array('post_id' => '42')) {
+                    return true;
+                }
+            }
+        }
+    },
+    
+    'Captures multiple named substrings' => function() use ($module) {
+        if ($route = $module->resolve('blog/posts/2012-07')) {
+            return $route->execute('get') === array('year'=>'2012', 'month'=>'07');
+        }
+    },
+    
+    'Inherits captured substrings from parent routes' => function() use ($module) {
+        if ($route = $module->resolve('blog/posts/2012-07/page2')) {
+            return $route->execute('get') === array('year'=>'2012', 'month'=>'07', 'page'=>'2');
+        }
+    },
+    
+    'Correctly resolves a sub-module' => function() use ($module) {
+        if ($route = $module->resolve('blog/posts/88/comments')) {
+            if ($route instanceof CommentModule) {
+                if ($route->execute('get') === 'comments') {
+                    return true;
+                }
+            }
+        }
+    },
+    
+    'Correctly resolves a route inside a sub-module' => function() use ($module) {
+        if ($route = $module->resolve('blog/posts/66/comments/submit')) {
+            if ($route->module instanceof CommentModule) {
+                if ($route->execute('get') === 'comment form') {
+                    return true;
+                }
+            }
+        }
+    },
+    
+    'Does not resolve when the route actively blocks the request (by returning false)' => function() use ($module) {
+        return ($module->resolve('blog/posts/99') === null)
+            && ($module->resolve('blog/posts/88') instanceof Route);
+    },
+    
+    'Routes, but does not execute when there is no action-method' => function() use ($module) {
+        if ($route = $module->resolve('blog')) {
+            return $route->execute('get') === false;
+        }
+    },
 );
 
 // Display test results:
@@ -128,17 +182,11 @@ $failed = 0;
 $number = 0;
 
 foreach ($tests as $name => $test) {
-    if (is_string($test)) {
-        $number = 0;
-        echo "\n" . $test . "\n\n";
-        continue;
-    } else {
-        $number += 1;
-    }
-
-    echo "  " . ($test === true ? 'PASS: ' : 'FAIL: ') . (is_int($name) ? "#$number" : $name) . "\n";
-
-    if ($test === true) {
+    $pass = $test() === true;
+    
+    echo "  " . ($pass ? 'PASS: ' : 'FAIL: ') . $name . "\n";
+    
+    if ($pass) {
         $passed += 1;
     } else {
         $failed += 1;
