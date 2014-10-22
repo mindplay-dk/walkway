925047afddc335677dcf6564f7be971a98dbf8a0
Rasmus Schultz <rasmus@mindplay.dk>
2014-10-22 21:29:11 +0200

    clean up

--- mindplay/walkway/Module.php
+++ mindplay/walkway/Module.php
@@ -21,6 +21,28 @@ use Closure;
  */
 class Module extends Route
 {
+    /**
+     * @var Closure[] hash where full regular expression => substitution closure
+     * @see init()
+     * @see preg_replace_callback()
+     */
+    public $substitutions = array();
+
+    /**
+     * @var string[] hash where symbol name => partial regular expression
+     * @see init()
+     */
+    public $symbols = array();
+
+    /**
+     * @var Closure event-handler for diagnostic messages
+     */
+    public $onLog;
+
+    /**
+     * @param Route  $parent
+     * @param string $token
+     */
     public function __construct(Route $parent = null, $token = '')
     {
         parent::__construct($this, $parent, $token);
@@ -34,40 +56,22 @@ class Module extends Route
     public function init()
     {
         $module = $this;
-        
+
         // define a default pattern-substitution with support for some common symbols:
-        
+
         $this->substitutions['/(?<!\(\?)<([^\:]+)\:([^>]+)>/'] = function($matches) use ($module) {
             if (isset($module->symbols[$matches[2]])) {
                 $matches[2] = $module->symbols[$matches[2]];
             }
-            
+
             return "(?<{$matches[1]}>{$matches[2]})";
         };
-        
+
         // define common symbols for the default pattern-substitution:
-        
+
         $this->symbols = array(
             'int' => '\d+',
             'slug' => '[a-z0-9-]+',
         );
     }
-    
-    /**
-     * @var Closure event-handler for diagnostic messages
-     */
-    public $onLog;
-    
-    /**
-     * @var Closure[] hash where full regular expression => substitution closure
-     * @see init()
-     * @see preg_replace_callback()
-     */
-    public $substitutions = array();
-    
-    /**
-     * @var string[] hash where symbol name => partial regular expression
-     * @see init()
-     */
-    public $symbols = array();
 }
